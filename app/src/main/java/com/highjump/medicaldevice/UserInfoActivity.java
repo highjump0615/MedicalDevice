d092e2e64cfa9d6db2014374465f4141dde9421d
highjump <lixiang901112@163.com>
2017-04-15 04:34:37 +0800

    Added user info edit page UI

--- app/src/main/java/com/highjump/medicaldevice/UserInfoActivity.java
+++ app/src/main/java/com/highjump/medicaldevice/UserInfoActivity.java
@@ -1,8 +1,12 @@
 package com.highjump.medicaldevice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class UserInfoActivity extends BaseActivity {
+import com.highjump.medicaldevice.utils.CommonUtils;
+
+public class UserInfoActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,23 @@ public class UserInfoActivity extends BaseActivity {
 
         // 初始化toolbar
         initToolbar();
+
+        Button button = (Button) findViewById(R.id.but_edit_info);
+        button.setOnClickListener(this);
+
+        button = (Button) findViewById(R.id.but_edit_pwd);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int nId = v.getId();
+
+        switch (nId) {
+            case R.id.but_edit_info:
+                // 跳转到修改会员信息页面
+                CommonUtils.moveNextActivity(UserInfoActivity.this, UserInfoEditActivity.class, false);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/highjump/medicaldevice/UserInfoEditActivity.java
@@ -0,0 +1,15 @@
+package com.highjump.medicaldevice;
+
+import android.os.Bundle;
+
+public class UserInfoEditActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userinfo_edit);
+
+        // 初始化toolbar
+        initToolbar();
+    }
+}
